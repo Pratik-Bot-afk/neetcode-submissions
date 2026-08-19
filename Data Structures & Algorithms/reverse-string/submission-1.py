76655158716646ac5bb3bd9c3c13d4e1ded0a910
Pratik-Bot-afk <pratikmanwatkar20@gmail.com>
2026-08-19 14:38:16 +0530

    Add: reverse-string - submission-1

---
+++ Data Structures & Algorithms/reverse-string/submission-1.py
@@ -0,0 +1,14 @@
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        p1 = 0
+        p2 = -1
+        for p1 in range(len(s)//2):
+            temp=s[p2]
+            s[p2]=s[p1]
+            s[p1]=temp
+            p1=p1+1
+            p2=p2-1
+
+        
+
+        
